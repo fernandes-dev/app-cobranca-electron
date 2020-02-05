0cd681f30dce28508aa731fd12d9306be8ca8f78
cn7polky <eduardo.yugan@gmail.com>
2020-02-05 14:14:33 -0400

    alteracao: <login> foi alterado para <entrar>

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -28,7 +28,7 @@
                     class="mb-2"
                     @click="login()"
                   >
-                    <div color="white">Login</div>
+                    <div color="white">Entrar</div>
                   </v-btn>
                 </v-card-text>
                 <v-spacer></v-spacer>
